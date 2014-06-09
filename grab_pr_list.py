9ac97330fd482c49c10da6687d9a58f1f6e04f59
Stefan van der Walt <stefan@sun.ac.za>
2014-06-09 18:14:15 +0200

    Switch to urllib3 to accommodate PythonAnywhere

--- grab_pr_list.py
+++ grab_pr_list.py
@@ -1,32 +1,37 @@
-import urllib
+import urllib3
+
 import json
 import os
 
+
+
 def fetch_PRs(user, repo, state='open'):
-    params = {'state': state,
+    fields = {'state': state,
               'per_page': 100,
               'page': 1}
+
+    config = {'user': user,
+              'repo': repo}
+    config = dict(config.items() + fields.items())
+
 
     data = []
     page_data = True
 
-    while page_data:
-        config = {'user': user,
-                  'repo': repo,
-                  'params': urllib.urlencode(params)}
+    url = 'https://api.github.com/repos/%(user)s/%(repo)s/pulls' % config
+    http = urllib3.PoolManager()
 
-        fetch_status = 'Fetching page %(page)d (state=%(state)s)' % params + \
+    while page_data:
+        fetch_status = 'Fetching page %(page)d (state=%(state)s)' % fields + \
                        ' from %(user)s/%(repo)s...' % config
         print fetch_status
 
-        f = urllib.urlopen(
-            'https://api.github.com/repos/%(user)s/%(repo)s/pulls?%(params)s' \
-            % config
-        )
+        response = http.request('GET', url, fields=fields,
+                                headers={'user-agent': 'scipy-procbuild/0.1'})
 
-        params['page'] += 1
+        fields['page'] += 1
 
-        page_data = json.loads(f.read())
+        page_data = json.loads(response.data)
 
         if 'message' in page_data and page_data['message'] == "Not Found":
             page_data = []
