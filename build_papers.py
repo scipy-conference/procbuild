4c1156b4db42700c4f1d3a0efddfbd3e9634cc69
Stefan van der Walt <stefan@sun.ac.za>
2014-06-19 17:30:51 +0200

    Add ability to trigger build of single paper

---
+++ build_papers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Schedule some or all papers for build
+
+from procbuild import papers, paper_queue
+import sys
+
+if len(sys.argv) > 1:
+    to_build = sys.argv[1:]
+else:
+    to_build = [nr for nr, info in papers]
+
+for p in to_build:
+    print "Placing %s in the build queue." % p
+    paper_queue.put(int(p))
+
+# Add sentinel to queue
+paper_queue.put(None)
+
