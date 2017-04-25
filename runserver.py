3ee78fedbf9d7bd882eee801fe38710aa4bb4f38
M Pacer <mpacer@berkeley.edu>
2017-04-25 16:48:49 -0700

    Move queue out of __init__ to avoid hanging; cc @stefanv & @Carreau

--- runserver.py
+++ runserver.py
@@ -2,7 +2,7 @@
 
 # -- SERVER CONFIGURATION -- (can be overridden from shell)
 
-config = (('MASTER_BRANCH', '2016'),
+config = (('MASTER_BRANCH', '2017'),
           ('ALLOW_MANUAL_BUILD_TRIGGER', '1'))
 
 # -- END SERVER CONFIGURATION --
@@ -12,9 +12,11 @@ for (key, val) in config:
     if not key in os.environ:
         os.environ[key] = val
 
-from procbuild import app
+from procbuild import app, monitor_queue
 from waitress import serve
 
+print('Monitoring build queue...')
+monitor_queue()
 serve(app, host='0.0.0.0', port=7000)
 
 # Without waitress, this is the call:
