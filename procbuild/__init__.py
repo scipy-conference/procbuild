45972006f9c1855ce1230710efa2ddb64a530053
mpacer <michaelpacer@gmail.com>
2017-04-28 15:21:30 -0700

    wrap multiline import with parens

--- procbuild/__init__.py
+++ procbuild/__init__.py
@@ -2,8 +2,8 @@ from __future__ import print_function, absolute_import
 
 import os
 
-from .server import app, log, papers, paper_queue, monitor_queue,
-    MASTER_BRANCH, ALLOW_MANUAL_BUILD_TRIGGER
+from .server import (app, log, papers, paper_queue, monitor_queue,
+    MASTER_BRANCH, ALLOW_MANUAL_BUILD_TRIGGER)
 
 # --- Customize these variables ---
 
