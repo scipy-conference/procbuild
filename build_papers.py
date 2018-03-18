a062a93d7b8d5180666179cddaf90d89297e552a
M Pacer <mpacer@berkeley.edu>
2018-03-18 01:37:07 -0700

    make build_papers work for one paper… even if it doesn't work for many

--- build_papers.py
+++ build_papers.py
@@ -2,15 +2,16 @@
 
 # Schedule some or all papers for build
 
-from procbuild import get_papers, paper_queue
+from procbuild.pr_list import get_papers 
+from procbuild.test_submit import submit_build_request
 import sys
 
 if len(sys.argv) > 1:
-    to_build = sys.argv[1:]
+    to_build = argv[1:]
+    import ipdb; ipdb.set_trace()
 else:
-    to_build = [int(nr) for nr, info in get_papers()]
+    to_build = [nr for nr, info in get_papers()]
 
 for p in to_build:
     print("Placing %s in the build queue." % p)
-
-    ### TODO: submit message to zmq bus
+    submit_build_request(p)
--- procbuild/test_submit.py
+++ procbuild/test_submit.py
@@ -1,17 +1,17 @@
 import zmq
-import random
 import json
 
 from .message_proxy import IN
-
 
 ctx = zmq.Context()
 socket = ctx.socket(zmq.PUSH)
 socket.connect(IN)
 
-
-if __name__ == "__main__":
-    # for i in range(10):
-    message = ['build_queue', json.dumps({'build_paper': 0})]
+def submit_build_request(nr):
+    message = ['build_queue', json.dumps({'build_paper': nr})]
     print('Submitting:', message)
     socket.send_multipart([m.encode('utf-8') for m in message])
+
+
+if __name__ == "__main__":
+    main(0)
